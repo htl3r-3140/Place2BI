f61c4a650586381760b5ec15ec0a155d9b3230a7
Elias Hirschl <3122@htl.rennweg.at>
2025-05-09 17:05:08 +0200

    Added header bar

--- Project/src/place2BI/Place2BI.java
+++ Project/src/place2BI/Place2BI.java
@@ -1,31 +1,65 @@
 package place2BI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import java.io.IOException;
 import java.util.Objects;
+import java.util.Random;
 
 public class Place2BI extends Application {
-    final int width = 1000;
-    final int height = 750;
+    final double height = Screen.getPrimary().getBounds().getHeight() * 0.9;
+    final double width = height * 0.8 * 16 / 9;
 
     @Override
     public void start(Stage stage) {
+        VBox vBoxLeft = new VBox(createText("2.6 S", Color.WHITE, false, 30));
+        vBoxLeft.setAlignment(Pos.CENTER_LEFT);
+        vBoxLeft.setPrefWidth(width * 0.15);
+
+        VBox vBoxCenter = new VBox(createText("Weiden am See", Color.web("#DD0000"), true, 35));
+        vBoxCenter.setAlignment(Pos.CENTER);
+        vBoxCenter.setPrefWidth(width * 0.6);
+
+        VBox vBoxRight = new VBox(createText("DU: 70 PT", Color.web("#00DD00"), false, 30), createText("KI: 71 PT", Color.web("#0000DD"), false, 30));
+        vBoxRight.setAlignment(Pos.CENTER_RIGHT);
+        vBoxRight.setPrefWidth(width * 0.15);
+
+        BorderPane borderPane = new BorderPane(vBoxCenter, null, vBoxRight, null, vBoxLeft);
+        BorderPane.setAlignment(borderPane, Pos.CENTER);
+        borderPane.setMaxSize(width, height * 0.2);
+        borderPane.setPadding(new Insets(0, width * 0.05, 0, width * 0.05));
+        borderPane.setStyle("-fx-background-color: black; -fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.5), 50, 0.5, 0, 0);");
+
         ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/maps/austria/Dblrw.png"))));
-        imageView.setFitWidth(width);
+        imageView.setFitHeight(height * 0.8);
         imageView.setPreserveRatio(true);
 
-        StackPane stackPane = new StackPane(imageView);
-        stackPane.setAlignment(Pos.BOTTOM_CENTER);
+        StackPane stackPane = new StackPane(imageView, borderPane);
+        StackPane.setAlignment(borderPane, Pos.TOP_CENTER);
+        StackPane.setAlignment(imageView, Pos.BOTTOM_CENTER);
 
-        Scene scene = new Scene(stackPane, width, height);
-        stage.setScene(scene);
+        stage.setScene(new Scene(stackPane, width, height));
         stage.setTitle("Place2BI");
+        stage.setResizable(false);
         stage.show();
+    }
+
+    public Text createText(String text, Color color, boolean underline, int size) {
+        Text createText = new Text(text);
+        createText.setFill(color);
+        createText.setUnderline(underline);
+        createText.setFont(Font.loadFont(Objects.requireNonNull(getClass().getResourceAsStream("/fonts/Posterama-Bold.ttf")), size));
+        return createText;
     }
 }
